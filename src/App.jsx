e800b17d8d856cf3dc9c71f5e3dc75c0d599eee2
hayyaFatima <155179686+hayyaFatima@users.noreply.github.com>
2024-03-20 16:44:15 +0500

    commit on Wednesday

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
-import Highlights from './components/Highlights'
+import Highlights from './components/HighLights'
 import Model from './components/Model'
 import Features from './components/Features'
 import HowItWorks from './components/HowltWorks'
