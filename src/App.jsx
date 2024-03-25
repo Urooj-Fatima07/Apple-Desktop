2e1586fb5e1cb656f2b3024903119138fa32967c
hayyaFatima <155179686+hayyaFatima@users.noreply.github.com>
2024-03-25 12:36:21 +0500

    Resolving Errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
-import Highlights from './components/HighLights'
+import Highlights from './components/Highlights'
 import Model from './components/Model'
 import Features from './components/Features'
 import HowItWorks from './components/HowltWorks'
